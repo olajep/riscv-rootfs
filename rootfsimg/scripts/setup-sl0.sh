f4073db33300f0452f04150849abbb93f09f448e
Ola Jeppsson <ola.jeppsson@gmail.com>
2019-02-28 15:54:24 -0500

    rootfsimg: scripts: setup-sl0.sh: Use ttyPL1
    Use the ttyPL1 device alias so we know we get the right serial console.

--- rootfsimg/scripts/setup-sl0.sh
+++ rootfsimg/scripts/setup-sl0.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-slattach -s 115200 -p cslip /dev/ttyS1 &
+slattach -s 115200 -p cslip /dev/ttyPL1 &
 /sbin/ifconfig sl0 192.168.42.2 pointopoint 192.168.42.1 up
 /sbin/ifconfig eth0 2>/dev/null || route add default gw 192.168.168.42.1

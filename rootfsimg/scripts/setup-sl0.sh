9d86ef10e3188b8d9a85519e7fcced97e9914eeb
Ola Jeppsson <ola.jeppsson@gmail.com>
2019-09-24 12:05:27 -0400

    rootfsimg: scripts: Fix script for setting up SLIP on ttyPL1

--- rootfsimg/scripts/setup-sl0.sh
+++ rootfsimg/scripts/setup-sl0.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 slattach -s 115200 -p cslip /dev/ttyPL1 &
+sleep 1
 /sbin/ifconfig sl0 192.168.42.2 pointopoint 192.168.42.1 up
-/sbin/ifconfig eth0 2>/dev/null || route add default gw 192.168.168.42.1
+sleep 1
+/sbin/ifconfig eth0 2>/dev/null || route add default gw 192.168.42.1
